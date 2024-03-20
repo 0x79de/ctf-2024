dc225d33f281feee23ceca42c278644de264981b
0x79de <0x79de@gmail.com>
2024-03-20 13:04:37 +0300

    initial commit

---
+++ start.exe/src/Challenge.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract Start {
+    function hello(string memory world) external pure returns (string memory) {
+    return world;
+    }
+}
